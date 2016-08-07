259bf0c01db3b1ea9d214eb754541202b5d7b239
Keiichi Watanabe <udon.watanabe@gmail.com>
2016-08-07 17:38:22 +0900

    `./vis.py both -i 101`

--- vis.py
+++ vis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
+import os
 import sys
 import math
 import numpy as np
@@ -42,7 +43,7 @@ def read_input(infile):
         lines.append((parse_point(p1), parse_point(p2)))
       return {"vs": vs, "polygons": ps, "lines": lines}
   except Exception:
-    print("Input Error:" + sys.exc_info()[0])
+    print("Input Error:", sys.exc_info()[0])
     raise
 
 def read_solution(infile):
@@ -64,7 +65,7 @@ def read_solution(infile):
         mps.append(parse_point(l))
       return {"vs": vs, "polygons": ps, "mappings": mps}
   except Exception:
-    print("Input Error:" + sys.exc_info()[0])
+    print("Input Error:", sys.exc_info()[0])
     raise
 
 def plot_input(data, filename):
@@ -115,9 +116,7 @@ def plot_solution(data, filename):
     mx_y = max(mx_y, v[1])
   cx = (mn_x + mx_x) / 2
   cy = (mn_y + mx_y) / 2
-  print(vs)
   vs = [(x -  cx, y -  cy) for (x, y) in vs]
-  print(vs)
   lines1 = []
   for poly in data["polygons"]:
     poly.append(poly[0])
@@ -133,6 +132,70 @@ def plot_solution(data, filename):
   #pl.savefig(filename[0:-3]+".png")
   pl.show()
 
+
+def plot_both(pfile, sfile):
+  fig = plt.figure()
+  data = read_input(pfile)
+
+  subplt = fig.add_subplot(2, 2, 1,aspect='equal')
+  lc = mc.LineCollection(data["lines"], linewidths=2)
+  (c_x, c_y) = (0, 0)
+  for [(x1, y1), (x2, y2)] in data["lines"]:
+    c_x += x1 + x2
+    c_y += y1 + y2
+  c_x /= len(data["lines"]) * 2.0
+  c_y /= len(data["lines"]) * 2.0
+  pl.axis([-1 + c_x, 1 + c_x, -1 + c_y, 1 + c_y])
+  pl.grid(True)
+  subplt.add_collection(lc)
+  subplt.margins(0.1)
+  #plt.savefig(filename[0:-3]+".png")
+
+
+  data = read_solution(sfile)
+  # 展開図
+  subplt = fig.add_subplot(2, 2, 3,aspect='equal')
+  vs = data["vs"]
+  lines1 = []
+  for poly in data["polygons"]:
+    poly.append(poly[0])
+    for i in range(len(poly) - 1):
+      lines1.append((vs[poly[i]], vs[poly[i + 1]]))
+  lc = mc.LineCollection(lines1, linewidths=2)
+  pl.axis([-0.5, 1.5, -0.5, 1.5])
+  subplt.add_collection(lc)
+  subplt.autoscale_view(True,True,True)
+  pl.grid(True)
+  plt.plot()
+
+  # シルエット
+  subplt1 = fig.add_subplot(2, 2, 4,aspect='equal')
+  vs = data["mappings"]
+  inf = float("inf")
+  mn_x, mn_y = inf, inf
+  mx_x, mx_y = -inf, -inf
+  for v in vs:
+    mn_x = min(mn_x, v[0])
+    mx_x = max(mx_x, v[0])
+    mn_y = min(mn_y, v[1])
+    mx_y = max(mx_y, v[1])
+  cx = (mn_x + mx_x) / 2
+  cy = (mn_y + mx_y) / 2
+  vs = [(x -  cx, y -  cy) for (x, y) in vs]
+  lines1 = []
+  for poly in data["polygons"]:
+    poly.append(poly[0])
+    for i in range(len(poly) - 1):
+      lines1.append((vs[poly[i]], vs[poly[i + 1]]))
+  lc = mc.LineCollection(lines1, linewidths=2)
+  subplt1.add_collection(lc)
+  subplt1.autoscale_view(True,True,True)
+  #pl.axis([-mn_x - 1,mx_x + 1,  -mn_y - 1, mx_y + 1])
+  pl.axis([-0.5, 1.5, -0.5, 1.5])
+  pl.grid(True)
+  plt.plot()
+  pl.show()
+
 def main():
   # for i in range(101):
   #   filename = "./problems/problem_"+str(i+1)+".in"
@@ -145,11 +208,16 @@ def main():
   # return
 
   parser = argparse.ArgumentParser()
-  parser.add_argument('mode', choices=['inp', 'sol'], help="'input' or 'sol'")
-  parser.add_argument("-i", "--input", type=str, required=True, help="file name (e.g. test.in)")
+  parser.add_argument('mode', choices=['inp', 'sol', 'both'], help="'input' or 'sol' or 'both'")
+  parser.add_argument("-i", "--input", type=str, help="file name (e.g. test.in)")
   args = parser.parse_args()
   infile = args.input
-  if args.mode == "inp":
+  if args.mode == "both":
+    CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+    pfile = CUR_DIR + "/problems/problem_{}.in".format(infile)
+    sfile = CUR_DIR + "/problems/solution_{}.out".format(infile)
+    plot_both(pfile, sfile)
+  elif args.mode == "inp":
     data = read_input(infile)
     plot_input(data, infile)
   else:
